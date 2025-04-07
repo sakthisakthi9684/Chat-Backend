ef136abbe791f8cdb183fbac734fb802709948a4
sakthisakthi9684 <sakthisakthi9684@gmail.com>
2025-04-07 17:55:25 +0530

    add ws

--- index.js
+++ index.js
@@ -22,17 +22,25 @@ app.use("/user", authRoutes);
 app.use("/msg", messageRoute);
 
 
-// Set up WebSocket server
-const wss = new WebSocket.Server({ server });
-
+const wss = new WebSocket.Server({ server, path: "/ws" });
 
 wss.on("connection", (ws) => {
-  logger.info("New client connected");
+  console.log("New client connected");
+
+  ws.on("message", (message) => {
+    // Broadcast to all clients
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
 
   ws.on("close", () => {
-    logger.info("Client disconnected");
+    console.log("Client disconnected");
   });
 });
+
 
 
 server.listen(5000, () => console.log("Server running on port 5000"));
